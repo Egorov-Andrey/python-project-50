b5c47738d808841b768606aea8a5a8f858290cba
Andrey Egorov <andreye89@mail.ru>
2025-11-27 11:18:35 +0300

    create package gendiff

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,16 @@
+import argparse
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Compares two configuration '
+    'files and shows a difference')
+    parser.add_argument('first file')
+    parser.add_argument('second file')
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
+
+
