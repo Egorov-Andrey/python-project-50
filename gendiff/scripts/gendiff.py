193dbc828ee4030e53e8d7caba4712fd631abc48
Andrey Egorov <andreye89@mail.ru>
2025-12-10 20:59:24 +0300

    create generate_diff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,7 @@
 import argparse
 
+from gendiff.generate_diff import generate_diff
+
 
 def main():
 
@@ -8,7 +10,11 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
-    parser.parse_args()
+    args = parser.parse_args()
+    first_file = args.first_file
+    second_file = args.second_file
+    diff_result = generate_diff(first_file, second_file)
+    print(diff_result)
 
 
 if __name__ == '__main__':
