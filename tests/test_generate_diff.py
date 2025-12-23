b55435ceebd5d5c351f0fe16786281510a75547a
Andrey Egorov <andreye89@mail.ru>
2025-12-23 09:24:00 +0300

    tests_genetate_diff.py

---
+++ tests/test_generate_diff.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from gendiff import generate_diff
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / "test_data" / filename
+
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+
+def test_generate_diff():
+    expected = read_file("result.txt")
+    actual = generate_diff("./tests/test_data/file1.json",
+                            "./tests/test_data/file2.json")
+    assert actual == expected
